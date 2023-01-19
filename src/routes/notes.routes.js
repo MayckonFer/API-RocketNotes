3f5f23ac260c64db1d94ad173934947c7702408c
Mayckon Fernandes <mayckonfernandes1@gmail.com>
2023-01-19 11:10:02 -0300

    api finished

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
 
 const NotesController = require("../controllers/NotesController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const notesRoutes = Router();
 
 const notesControlles = new NotesController();
 
-notesRoutes.get("/", notesControlles.index);
-notesRoutes.post("/:user_id", notesControlles.create);
+notesRoutes.use(ensureAuthenticated);
+
+notesRoutes.post("/", notesControlles.create);
 notesRoutes.get("/:id", notesControlles.show);
 notesRoutes.delete("/:id", notesControlles.delete);
+notesRoutes.get("/", notesControlles.index);
 
 module.exports = notesRoutes;
